2dfd230bc54e8b290e317b0b3ba7b4c80596db7f
Leonardo Faria <66142358+LeonardoFariaOliveira@users.noreply.github.com>
2023-03-25 11:33:29 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Testando o merge das branchs
+          Testando o merge das branchs, de novo - 2
         </p>
         <a
           className="App-link"
